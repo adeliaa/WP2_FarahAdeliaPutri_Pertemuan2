b91c923ca7a592b2facff2b6802d00f6e67616af
adeliaa <farah.adelia09@gmail.com>
2021-10-14 09:15:40 +0700

    tugas pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Farah Adelia Putri
+ Saya tinggal di daerah bekasi
+ olah raga yang saya sukai adalah bersepeda";
+ }
+}
